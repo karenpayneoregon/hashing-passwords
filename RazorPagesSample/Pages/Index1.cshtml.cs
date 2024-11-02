ce7ede9dc331a2f822e4355c206bdb651f849386
Karen Payne <paynekaren@comcast.net>
2024-11-02 13:23:27 -0700

    Add new login page and update existing settings
    - Minor formatting adjustments in `Index.cshtml` for password input field alignment and removal of redundant lines.
    - Reorganized `launchSettings.json` for better readability; moved `iisSettings` section, adjusted `dotnetRunMessages` and `applicationUrl`, and added `launchUrl` to `https` profile.
    - Created new Razor Page `Index1.cshtml` with a login form, password visibility toggle, and submit button.
    - Added `Index1.cshtml.cs` with `Index1Model` class for user authentication, including `OnGet` and `OnPostValidateUser` methods.

---
+++ RazorPagesSample/Pages/Index1.cshtml.cs
@@ -0,0 +1,49 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using RazorPagesSample.Data;
+using RazorPagesSample.Interfaces;
+using RazorPagesSample.Models;
+
+#pragma warning disable CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider declaring as nullable.
+
+// ReSharper disable ConvertConstructorToMemberInitializers
+#pragma warning disable CS8602 // Dereference of a possibly null reference.
+
+namespace RazorPagesSample.Pages;
+
+public class Index1Model(Context context, IAuthentication authentication) : PageModel
+{
+    public void OnGet()
+    {
+        CurrentUser = new User() { Name = "Karen", Password = "password" };
+    }
+
+    // Set user to an existing record in the database
+    [BindProperty]
+    public User CurrentUser { get; set; }
+
+    /// <summary>
+    /// Text to show for authentication status
+    /// </summary>
+    public string Message { get; set; } = "";
+
+    /// <summary>
+    /// Validates the current user by checking the provided password against the stored password in the database.
+    /// Authentication
+    /// </summary>
+    /// <remarks>
+    /// This method uses the BCrypt library to verify the password and logs the authentication result.
+    /// </remarks>
+    public void OnPostValidateUser()
+    {
+
+        var (authenticated, id) = authentication.ValidateUser(CurrentUser!, context);
+
+        Log.Information(authenticated ?
+            "{Id,-3} User {Name} authenticated" :
+            "User {Name} not authenticated", id, CurrentUser.Name);
+
+        Message = authenticated ? "Authenticated" : "Not authenticated";
+    }
+
+}
